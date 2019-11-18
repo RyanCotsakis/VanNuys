066b6335c26afbc3f0b830f0155987fb6978d79a
ryan <ryan.cotsakis@polymtl.ca>
2019-11-18 17:12:06 +0100

    Observed seasonal effects

--- main.R
+++ main.R
@@ -3,15 +3,17 @@ load("VanNuys.RData")
 date = seq(as.Date("1973-03-31"),as.Date("2015-05-31"),by="days") # Creates a date vector
 wind = as.numeric(VanNuys[,1])
 ffwi = as.numeric(VanNuys[,2])
-# day = seq(1,15402)
-# month=months(date)
 
-# plot(date,wind)
-# plot(date,ffwi)
+# day = seq(1,15402)
+month=months(date, abbreviate=FALSE)
+month_as_numbers = match(month,month.name)
+
+plot(month_as_numbers,wind)
+plot(month_as_numbers,ffwi)
 
 wind_measured = !is.na(wind) # Boolean vector: if wind was measured
 ffwi_measured = !is.na(ffwi) # Boolean vector: if ffwi was measured
 
 # Auto Correlation Functions
-acf(wind[wind_measured])
-acf(ffwi[ffwi_measured])
+acf(wind[wind_measured], lag.max = 400)
+acf(ffwi[ffwi_measured], lag.max = 400)
