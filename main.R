048e1cb5d0daee43233d6b6c2a3f6eebc834d1d7
Victor Gonzalo <victorgonzal@gmx.de>
2019-12-01 15:34:07 +0100

    univariate is moving, baby

--- main.R
+++ main.R
@@ -11,8 +11,14 @@ date = seq(as.Date("1973-03-31"),as.Date("2015-05-31"),by="days") # Creates a da
 wind = as.numeric(VanNuys[,1])
 ffwi = as.numeric(VanNuys[,2])
 
+# Analyze NAs
+plot(date, is.na(ffwi))
+plot(date, is.na(wind))
+
 # day = seq(1,15402)
 month=months(date, abbreviate=FALSE)
+year=as.numeric(format(date,"%Y"))
+day_of_year=as.numeric(format(date,"%j"))
 month_as_numbers = match(month,month.name)
 
 plot(date,wind)
@@ -27,7 +33,7 @@ ffwi_measured = !is.na(ffwi) # Boolean vector: if ffwi was measured
 # Auto Correlation Functions
 acf(wind[wind_measured], lag.max = 400)
 acf(ffwi[ffwi_measured], lag.max = 400)
-
+exiplot(ffwi[ffwi_measured], tlim=c(50,90))
 
 #Fit GEV-wind
 wind_maxima = apply(matrix(wind[wind_measured], ncol=365), 1, max)
@@ -38,7 +44,14 @@ fit_GEV_wind
 plot(profile(fit_GEV_wind))
 
 #Fit GEV-ffwi
-ffwi_maxima = apply(matrix(ffwi[ffwi_measured], ncol=365), 1, max)
+ffwi_maxima = array(0,43)
+for(j in 1:43){
+  ffwi_maxima[j]=max(ffwi[ffwi_measured][year==1972+j])
+}
+ffwi_biggest = matrix(0,nrow=43,ncol=10)
+for(j in 1:43){
+  ffwi_biggest[j,1:10] = order(ffwi[ffwi_measured][year==1972+j])[1:10]
+}
 fit_GEV_ffwi = fgev(ffwi_maxima,method="Nelder-Mead")
 qqplot(qgumbel(c(1:42)/43),ffwi_maxima)
 plot(fit_GEV_ffwi)
@@ -55,5 +68,66 @@ qu.max = quantile(ffwi[ffwi_measured],(length(ffwi[ffwi_measured])-30)/length(ff
 mrlplot(ffwi[ffwi_measured], tlim=c(qu.min, qu.max))
 tcplot(ffwi[ffwi_measured],tlim=c(qu.min, qu.max))
 
-th = 80
-fpot(dat,threshold=th)
+th = 62 #TODO: check threshold assumptions 
+fit_GPD_ffwi = fpot(ffwi[ffwi_measured],threshold=th)
+
+# check fit
+plot(fit_GPD_ffwi) #prob. plot wiggling out, QQ wiggling, density ok, return wiggling
+#wiggling because of discreteness, step distance 3 dealt with in next chapter
+
+#exceedence rate
+plot(day_of_year[ffwi>th],ffwi[ffwi>th])
+exceedence_per_day = array(0,366)
+for(j in 1:366){
+  numerator = sum(ffwi[day_of_year == 3 && ffwi_measured])
+  exceedence_per_day[j] = sum(ffwi[day_of_year==j & ffwi>th])/sum(ffwi[day_of_year==j])
+}
+
+
+# profile log-likelihood for both parameters
+par(mfrow=c(1,2))
+plot(profile(fit_GPD_ffwi))
+abline(v=0,col=2,lty=2) # 0 not inside of the confidence intervals
+par(mfrow=c(1,1))
+
+# fit shape=0
+fit_GPD_ffwi_gum = fpot(ffwi[ffwi_measured], threshold=th, shape=0)
+plot(fit_GPD_ffwi_gum) # almost same, maybe a little worse
+
+# deal with varying variance
+plot(date[ffwi>th],ffwi[ffwi>th])
+fit_GPD_ffwi_linkfit = gpd.fit(ffwi[ffwi_measured],threshold=th,ydat=as.matrix(scale(date)),sigl=1,siglink=exp)
+gpd.diag(fit_GPD_ffwi_linkfit) #TODO: ??
+
+##
+## eliminating bad data & discreteness
+# create elimination vectors
+good_dates = (year>1992) & (ffwi_measured) & ((day_of_year<150) | (day_of_year>280))
+plot(good_dates)
+
+#eliminate discreteness
+ffwi_jittered = jitter(ffwi,amount = 1.5)
+ffwi=ffwi_jittered
+
+# choose threshold, fit GPD for good dates and check fit
+qu.min = quantile(ffwi[good_dates], 0.5)
+qu.max = quantile(ffwi[good_dates],(length(ffwi[good_dates])-30)/length(ffwi[good_dates]))
+mrlplot(ffwi[good_dates], tlim=c(qu.min, qu.max))
+tcplot(ffwi[good_dates],tlim=c(qu.min, qu.max))
+
+th = 55 #TODO: check threshold assumptions 
+fit_GPD_ffwi = fpot(ffwi[good_dates],threshold=th)
+
+# check fit
+plot(fit_GPD_ffwi) #prob. plot wiggling out, QQ wiggling, density ok, return wiggling
+
+
+##
+# fit PP-ffwi
+#choose threshold as before, compare with...
+u = quantile(ffwi[ffwi_measured],0.95)
+fit_PP_ffwi = fpot(ffwi[good_dates],threshold=th,model="pp")
+plot(fit_PP_ffwi) #same as before
+
+##
+# r-largest-value approach
