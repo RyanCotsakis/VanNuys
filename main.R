0fc0b045ca725e0d5fbeece2306b6fdc5b49e369
ryan <ryan.cotsakis@polymtl.ca>
2019-12-01 17:37:49 +0100

    Skeleton of everything we need. Started Bivariate

--- main.R
+++ main.R
@@ -2,8 +2,11 @@
 #library(readr)
 #Sys.setlocale(locale="English")
 
+# TODO: Check dates of maxima to see if there is independence between years (eg Dec 31 to Jan 1)
+
 library(evd)
 library(evdbayes)
+library(ismev)
 
 load("VanNuys.RData")
 
@@ -20,23 +23,33 @@ month=months(date, abbreviate=FALSE)
 year=as.numeric(format(date,"%Y"))
 day_of_year=as.numeric(format(date,"%j"))
 month_as_numbers = match(month,month.name)
-
-plot(date,wind)
-plot(date,ffwi)
-
-plot(month_as_numbers,wind)
-plot(month_as_numbers,ffwi)
+# 
+# plot(date,wind)
+# plot(date,ffwi)
+# 
+# plot(month_as_numbers,wind)
+# plot(month_as_numbers,ffwi)
 
 wind_measured = !is.na(wind) # Boolean vector: if wind was measured
 ffwi_measured = !is.na(ffwi) # Boolean vector: if ffwi was measured
+wind[!wind_measured] = 0
+ffwi[!ffwi_measured] = 0
 
 # Auto Correlation Functions
 acf(wind[wind_measured], lag.max = 400)
 acf(ffwi[ffwi_measured], lag.max = 400)
 exiplot(ffwi[ffwi_measured], tlim=c(50,90))
+# 
+# # Get rid of ALL NA's
+# wind = wind[wind_measured]
+# ffwi = ffwi[ffwi_measured]
+# 
 
 #Fit GEV-wind
-wind_maxima = apply(matrix(wind[wind_measured], ncol=365), 1, max)
+wind_maxima = array(0,43)
+for(j in 1:43){
+  wind_maxima[j]=max(wind[year==1972+j])
+}
 fit_GEV_wind = fgev(wind_maxima,method="Nelder-Mead")
 qqplot(qgumbel(c(1:42)/43),wind_maxima)
 plot(fit_GEV_wind)
@@ -46,12 +59,28 @@ plot(profile(fit_GEV_wind))
 #Fit GEV-ffwi
 ffwi_maxima = array(0,43)
 for(j in 1:43){
-  ffwi_maxima[j]=max(ffwi[ffwi_measured][year==1972+j])
+  ffwi_maxima[j]= max(ffwi[year == 1972 +j])
 }
+fit_GEV_ffwi = fgev(ffwi_maxima,method="Nelder-Mead")
+qqplot(qgumbel(c(1:42)/43),ffwi_maxima)
+plot(fit_GEV_ffwi)
+fit_GEV_wind
+plot(profile(fit_GEV_wind))
+
+## R largest:
+r = 9
 ffwi_biggest = matrix(0,nrow=43,ncol=10)
 for(j in 1:43){
-  ffwi_biggest[j,1:10] = order(ffwi[ffwi_measured][year==1972+j])[1:10]
+  ffwi_biggest[j,1:10] = sort(ffwi[year==1972+j], decreasing = TRUE)[1:10]
 }
+ffwi_biggest = ffwi_biggest[9:42 , 1:r]
+ffwi_biggest.vec = as.vector(t(ffwi_biggest))
+year.vec = rep(1:34, each=r)
+fit1 = rlarg.fit(xdat = ffwi_biggest)
+par(mfrow = c(3,3))
+rlarg.diag(fit1)
+
+#gumble profile
 fit_GEV_ffwi = fgev(ffwi_maxima,method="Nelder-Mead")
 qqplot(qgumbel(c(1:42)/43),ffwi_maxima)
 plot(fit_GEV_ffwi)
@@ -130,4 +159,19 @@ fit_PP_ffwi = fpot(ffwi[good_dates],threshold=th,model="pp")
 plot(fit_PP_ffwi) #same as before
 
 ##
-# r-largest-value approach
+## --- CHAPTER 3: BIVARIATE ANALYSIS ---
+##
+
+# 3.1 - Maxima
+y = c(1973:2015)
+M = cbind(y, ffwi_maxima, wind_maxima)
+M = M[9:42,]
+M_all = cbind(year, ffwi, wind)
+
+fit_bi_gev = fbvevd(M[,-1], model = "log") # TODO: try different values of model
+plot(fit_bi_gev, mar = 2) # "which" argument tells which plots to generate
+plot(fit_bi_gev)
+
+q_ffwi = quantile(ffwi, 0.95)
+q_wind = quantile(wind, 0.95)
+fit_bi_pot = fbvpot(M_all[,-1], c(q_ffwi, q_wind), model = "log")
